c92e761bb1749ba8227dc581e32d8e9598949445
Unknown <page.hogan@gmail.com>
2019-10-13 19:34:42 +1000

    Added classes
    Updated Ignore

--- Models/SignwordDataModel.cs
+++ Models/SignwordDataModel.cs
@@ -1,10 +1,11 @@
-﻿namespace AuslanAPI.Models
+﻿using static AuslanAPI.Models.Enums;
+
+namespace AuslanAPI.Models
 {
     public class SignwordDataModel : BaseDataModel
     {
         public string Signword { get; set; }
         public SignCategory Category { get; set; }
         public string Image { get; set; }
-
     }
 }
